a8bbfab2423784fb5816f2e567ece1d0073e3513
Lenin Arce <larceh@unmsm.edu.pe>
2025-04-15 20:53:28 -0500

    primer commit

---
+++ p1/src/p1/V1.java
@@ -0,0 +1,78 @@
+package p1;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class V1 extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JLabel lblNewLabel;
+	private JTextField txtNom;
+	private JButton btnNewButton;
+	private JScrollPane scrollPane;
+	private JTextArea txtS;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					V1 frame = new V1();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public V1() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 505, 330);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		{
+			lblNewLabel = new JLabel("Nombre");
+			lblNewLabel.setBounds(21, 26, 62, 13);
+			contentPane.add(lblNewLabel);
+		}
+		{
+			txtNom = new JTextField();
+			txtNom.setBounds(79, 23, 96, 19);
+			contentPane.add(txtNom);
+			txtNom.setColumns(10);
+		}
+		{
+			btnNewButton = new JButton("Procesar");
+			btnNewButton.setBounds(222, 22, 85, 21);
+			contentPane.add(btnNewButton);
+		}
+		{
+			scrollPane = new JScrollPane();
+			scrollPane.setBounds(26, 120, 281, 115);
+			contentPane.add(scrollPane);
+			{
+				txtS = new JTextArea();
+				scrollPane.setViewportView(txtS);
+			}
+		}
+	}
+}
